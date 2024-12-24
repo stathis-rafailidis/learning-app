c63a3f8ce5e6020c9d05bab7cc9966e6ae3a2922
stathisraf <61472058+stathisraf@users.noreply.github.com>
2024-12-24 21:57:04 +0200

    initial commit

---
+++ flask-server/server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/members")
+def members():
+    return {"mem": ['mem1', 'mem2', 'mem3']}\
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
